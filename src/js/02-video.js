827464033a7d627bf6345352ed4d601a261707e7
Vladislav Zaitsev <vladislav.zaytcev84@gmail.com>
2021-11-16 16:17:03 +0200

    hw 2.4

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,30 +1,21 @@
   import { save, load } from './storage';
-  import throttle from 'lodash.throttle'
+  import Player from '@vimeo/player';
+  import throttle from 'lodash.throttle';
+
   const iframe = document.querySelector('iframe');
-  const player = new Vimeo.Player(iframe);
-
-  // const getValue = localStorage.getItem("videoplayer-current-time")
-  // console.log(getValue);
-  // const parseValue = JSON.parse(getValue)
-
+  const player = new Player(iframe);
   const timeupdateData = load("videoplayer-current-time")
 
   if(timeupdateData){
-    player.setCurrentTime(JSON.parse(timeupdateData).seconds)
+    player.setCurrentTime(timeupdateData)
   }
 
-  // player.setCurrentTime(parseValue.seconds)
+  player.on('timeupdate', throttle(function(data) {
+console.log(data.seconds)
+    save("videoplayer-current-time", JSON.stringify(data.seconds));
 
-  player.on('timeupdate', function(data) {
-console.log(data)
-    // localStorage.setItem("videoplayer-current-time", JSON.stringify(data))
 
-    throttle(() => {
-      save("videoplayer-current-time",  JSON.stringify(data));
-      console.log("Scroll handler call every 300ms");
-    }, 100)
-
-});
+}, 1000));
 
 
 // video.currentTime="seconds"
@@ -34,4 +25,3 @@ console.log(data)
 
 
 
-// </script
